2e9232b828f1c066b2e9682e17135f54eb1e178c
Kevin Reynolds <kevin.reynolds@nimbletank.com>
2018-10-28 19:25:36 +0000

    save

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://kevin:kevin319!@ds143293.mlab.com:43293/sensationmm-devconnector'  
+  mongoURI: 'mongodb://kevin:kevin319!@ds143293.mlab.com:43293/sensationmm-devconnector',
+  secretOrKey: 'secret'  
 }
